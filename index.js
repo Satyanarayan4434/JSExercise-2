b354736db0ad49aa0c3c1538c846d0dc285d07b8
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-05-29 12:26:16 +0530

    solving JS exercise - 2

---
+++ index.js
@@ -0,0 +1,60 @@
+//1st problem
+    // function greet(name){
+    //     console.log(name);
+    // }
+    // greet("satyanarayan");
+//2nd problem
+    // function getSquare(num){
+    //     let square = num**2;
+    //     return square;
+    // }
+    // console.log(getSquare(6));
+//3rd problem
+    // function countLetter(str){
+    //     let count = {}
+    //     for(let i=0; i<=str.length; i++){
+    //         let char = str[i];
+    //         if(count[char]){
+    //             count[char]++;
+    //         }
+    //         else{
+    //             count[char] = 1;
+    //         }
+    //     }
+    //     return count;
+    // }
+    // console.log(countLetter("satyanarayan"));
+//4th problem
+    // function createCounter(){
+    //     let count = 0
+    //     return () => {
+    //         count++;
+    //         return count;
+    //     }
+    // }
+    // let counter = createCounter();
+    // console.log(counter());
+    // console.log(counter());
+//5th problem
+    // function sumEvenNumber(arr){
+    //     let sum = 0;
+    //     for(let value of arr){
+    //         if(value % 2===0){
+    //             sum +=value;
+    //         }
+    //     }
+    //     return sum;
+    // }
+    // let arr = [20,20,60,39,10,87,49];
+    // console.log(sumEvenNumber(arr));
+//6th problem
+    // function sumOfArray(arr){
+    //     let sum = 0;
+    //     for(let value of arr){
+    //         sum += value;
+    //     }
+    //     return sum;
+    // }
+    // let arr = [1,2,3,4,5,6,7,8,9,10];
+    // console.log(sumOfArray(arr));
+//7th pro
